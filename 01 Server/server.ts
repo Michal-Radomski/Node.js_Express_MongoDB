eee0bbaf162dd5a57f321f26ee72934023712d42
Michal Radomski <michalra@op.pl>
2022-04-03 21:04:43 +0200

    Task1 done

---
+++ 01 Server/server.ts
@@ -0,0 +1,7 @@
+const http = require("http"); //import http from "http"
+const server = http.createServer((_req: any, res: any) => {
+  console.log("_req.url", _req.url, "res", res);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end("<h1>Hello Node!</h1>");
+});
+server.listen(5500, "127.0.0.1", () => console.log("server started"));
