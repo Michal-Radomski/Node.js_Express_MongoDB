eea22bea866bed972d0b5039f74c22edf46f2dba
Michal Radomski <michalra@op.pl>
2022-04-03 22:20:17 +0200

    task1 - next commit

--- 01 Server/server.ts
+++ 01 Server/server.ts
@@ -1,6 +1,10 @@
+const os = require("os");
+console.log("os.totalmem():", os.totalmem());
+console.log("os.type():", os.type());
+
 const http = require("http"); //import http from "http"
 const server = http.createServer((_req: any, res: any) => {
-  console.log("_req.url", _req.url, "res", res);
+  // console.log("_req.url", _req.url, "res", res);
   res.writeHead(200, {"Content-Type": "text/html"});
   res.end("<h1>Hello Node!</h1>");
 });
