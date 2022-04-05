3db2f1b1c1e0f35ee0de95174a5696728330839f
Michal Radomski <michalra@op.pl>
2022-04-05 20:41:11 +0200

    Modules - commit2

--- 02 Moduły/app.ts
+++ 02 Moduły/app.ts
@@ -1,13 +1,18 @@
 var colors = require("colors/safe");
 const notes = require("./notes.ts");
-console.log("notes:", notes);
 
-console.log(colors.green("hello")); // outputs green text
-console.log(colors.red.underline("i like cake and pies")); // outputs red underlined text
-console.log(colors.inverse("inverse the color")); // inverses the color
-console.log(colors.rainbow("OMG Rainbows!")); // rainbow
-console.log(colors.trap("Run the trap")); // Drops the bass
+// const mod = require("module");
+// console.log("mod.wrapper:", mod.wrapper);
+
+console.log("notes:", notes);
+console.log("notes.txt:", notes.txt);
+
+console.log(colors.yellow("hello")); // outputs green text
+// console.log(colors.red.underline("i like cake and pies")); // outputs red underlined text
+// console.log(colors.inverse("inverse the color")); // inverses the color
+// console.log(colors.rainbow("OMG Rainbows!")); // rainbow
+// console.log(colors.trap("Run the trap")); // Drops the bass
 // console.log("colors: ", colors);
 
 console.log("Jestem w module app");
-console.log("Nodemon was added and is being used");
+console.log("Nodemon was added and \nis being used");
--- 02 Moduły/notes.ts
+++ 02 Moduły/notes.ts
@@ -1,1 +1,9 @@
 console.log("Jestem w module notes");
+
+console.log("exports1:", exports === module.exports);
+module.exports = {txt: "return by module.exports"};
+// console.log("exports:", exports);
+// console.log("exports2:", exports === module.exports);
+// console.log("__dirname:", __dirname);
+// console.log("require:", require);
+// console.log("module:", module);
