ef53794688abd6a29c6fbdc783ac66a11ddc8741
Michal Radomski <michalra@op.pl>
2022-04-07 14:43:49 +0200

    ToDoList - commit2

--- 03b ToDoApp/app.ts
+++ 03b ToDoApp/app.ts
@@ -44,12 +44,48 @@ const handleData = (type: number, title: string | null) => {
   if (type === 1 || type === 2) {
     const isExisted = tasks.find((task: {title: string | null}) => task.title === title) ? true : false;
     if (type === 1 && isExisted) {
-      return console.log(colors.yellow("Takie zadanie już istnieje"));
+      return console.log(colors.yellow.bgBlue("Takie zadanie już istnieje"));
     } else if (type === 2 && !isExisted) {
-      return console.log(colors.yellow("Nie mogę usuwać zadania, które nie istnieje"));
+      return console.log(colors.yellow.bgMagenta("Nie mogę usuwać zadania, które nie istnieje"));
     }
   }
+
+  let dataJSON = "";
+
   switch (type) {
+    case 1:
+      // console.log("Dodaję zadanie");
+      const id = tasks.length + 1;
+      tasks.push({id: id, title: title});
+      console.log("tasks:", tasks);
+      dataJSON = JSON.stringify(tasks);
+      console.log("dataJSON:", dataJSON);
+      fs.writeFileSync("./dataDB.json", dataJSON);
+      console.log(colors.white.bgGreen(`Dodaję zadanie do bazy: ${title}`));
+      break;
+    case 2:
+      // console.log("Usuwam zadnie");
+      const index: number = tasks.findIndex((task: {title: string | null}) => task.title === title);
+      tasks.splice(index, 1);
+      console.log("tasks:", tasks);
+      dataJSON = JSON.stringify(tasks);
+      fs.writeFile("./dataDB.json", dataJSON, "utf8", (err: string) => {
+        if (err) throw err;
+      });
+      console.log(colors.white.bgRed(`Zadanie zostało usunięte z bazy: ${title}`));
+      break;
+    case 3:
+      console.log(`Lista zadań do zrobienie obejmuje ${tasks.length} pozycji. Do zrobienia masz: `);
+      if (tasks.length > 0) {
+        tasks.forEach((task: {title: string | null}, index: number) => {
+          if (index % 2) {
+            return console.log(colors.green(task.title));
+          } else {
+            return console.log(colors.yellow(task.title));
+          }
+        });
+      }
+      break;
   }
 };
 
