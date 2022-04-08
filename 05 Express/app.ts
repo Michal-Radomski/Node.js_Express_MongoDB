7c1b088a795c3cd15791038869e2ea39d974456b
Michal Radomski <michalra@op.pl>
2022-04-08 23:09:01 +0200

    Express.js - commit2

--- 05 Express/app.ts
+++ 05 Express/app.ts
@@ -30,18 +30,42 @@ app.get(
     console.log("req.query:", req.query);
     const {name, surname} = req.query;
     console.log(`Hello ${name} ${surname}`);
-    res.send("<h1>Hello World!</h1>");
+    res.send("<h1>Hello World!</h1>"); //* send = write + end (z Node.js) Dodaje też nagłówek z kodowaniem
+    //* Dane wejściowe mogą być text/html, array/object lub Buffer
     if (req.protocol !== "https") {
       console.log("Protokół niezabezpieczony");
     }
   }
 );
 
-app.all("/hi", (req: {method: string}, res: {send: (arg0: string) => void}) => {
+app.get("/abc", (_req: any, res: {location: (arg0: string) => void; sendStatus: (arg0: number) => void}) => {
+  res.location("/123");
+  res.sendStatus(302); //* przekierowanie trwałe
+});
+
+app.get("/def", (_req: any, res: {redirect: (arg0: number, arg1: string) => void}) => {
+  res.redirect(301, "https://www.google.com"); //* przekierowanie nietrwałe
+});
+
+app.all("/hi", (req: {method: string}, res: {write: (arg0: string) => void; end: () => void}) => {
   console.log("req.method:", req.method);
-  res.send("<h1>Hi World!</h1>");
+  res.write("<h1 style='color:red'>Hi World!</h1>");
+  res.end();
 });
 
 app.listen(port, "localhost", () => {
   console.log("Server is listening at port:", port);
+});
+
+//* Podstawowe ścieżki REST - i tak dla wszystkich poleceń: GET, POST, PATH, DELETE
+// app.post("/", (_req: string) => {
+//   console.log("Dodawanie nowej osoby");
+// });
+
+//* Parametry ścieżek
+// app.post("/:name/:surname", (_req: string) => {
+//   console.log("Dodawanie nowej osoby");
+// });
+app.get("/article/:id/:tile?", (req: {params: string}) => {
+  console.log("req.params:", req.params);
 });
