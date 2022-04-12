3c01121c54a7046368f058bcc8a85cbc1f9bc753
Michal Radomski <michalra@op.pl>
2022-04-12 21:51:07 +0200

    commit1

--- 07 BackEnd App/src/routes/index.ts
+++ 07 BackEnd App/src/routes/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (_req, res, _next) {
-  res.render("index", { title: "Express" });
+router.get("/", (_req, res, _next) => {
+  console.log("Server is running - index.ts");
+  res.render("index", {title: "Express.JS"});
 });
 
 export default router;
--- 07 BackEnd App/src/routes/users.ts
+++ 07 BackEnd App/src/routes/users.ts
@@ -2,8 +2,13 @@ import express from "express";
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (_req, res, _next) {
+// router.get("/", function (_req, res, _next) {
+//   res.send("respond with a resource");
+// });
+
+router.get("/", (_req, res, _next) => {
   res.send("respond with a resource");
+  console.log("Server is running - users.ts");
 });
 
 export default router;
