df063485d34b5388b5306651ebd53f48d87db3f5
Michal Radomski <michalra@op.pl>
2022-04-13 23:52:30 +0200

    Quiz done

--- 07 BackEnd App/src/routes/quiz.ts
+++ 07 BackEnd App/src/routes/quiz.ts
@@ -1,10 +1,31 @@
 import express from "express";
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", (_req, res, _next) => {
-  // console.log("Server is running - index.ts");
-  res.render("quiz", {title: "Quiz"});
+import Quiz from "../../models/quiz";
+
+router.get("/", (req, res, _next) => {
+  const show = !req!.session!.vote;
+
+  Quiz.find({}, (_err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+    res.render("quiz", {title: "Quiz", data, show, sum});
+  });
+});
+
+router.post("/", (req, res) => {
+  const id = req.body.quiz;
+
+  Quiz.findOne({_id: id}, (_err: string, data: {vote: number; save: (arg0: (_err: string) => void) => void}) => {
+    data.vote = data.vote + 1;
+    data.save((_err: string) => {
+      req!.session!.vote = 1;
+
+      res.redirect("/quiz");
+    });
+  });
 });
 
 export default router;
