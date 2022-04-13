f5a94acb22d3f527d4e043640086bdc22552a6f8
Michal Radomski <michalra@op.pl>
2022-04-13 23:23:58 +0200

    news filter added

--- 07 BackEnd App/src/routes/news.ts
+++ 07 BackEnd App/src/routes/news.ts
@@ -1,10 +1,24 @@
 import express from "express";
 var router = express.Router();
 
+import News from "../../models/news";
+
 /* GET home page. */
-router.get("/", (_req, res, _next) => {
+router.get("/", (req, res, _next) => {
   // console.log("Server is running - index.ts");
-  res.render("news", {title: "News"});
+
+  console.log("req.query:", req.query);
+  const search = req.query.search || "";
+
+  const findNews = News
+    //@ts-ignore
+    .find({title: new RegExp(search.trim(), "i")})
+    .sort({created: -1}); //* Sortowanie malejąco
+
+  findNews.exec((_err, data) => {
+    // console.log("data:", data);
+    res.render("news", {title: "News", data, search});
+  });
 });
 
 export default router;
