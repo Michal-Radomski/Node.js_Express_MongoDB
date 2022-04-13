ff10113f54ad930942c74b242e4a5d7887ca83de
Michal Radomski <michalra@op.pl>
2022-04-14 00:51:25 +0200

    last commit

---
+++ 07 BackEnd App/src/config.ts
@@ -0,0 +1,11 @@
+// module.exports = {
+//   // db: 'mongodb+srv://admin:p6tdGLXJBWOOOYSy@cluster0-0dwhe.mongodb.net/test?retryWrites=true',
+//   keySession: ['Twój_klucz'],
+//   maxAge: 10 * 60 * 1000 // 10 minutes
+// }
+
+export const config = {
+  db: "mongodb+srv://HentyiOsmy:cKtGe0MCKWuuJNQ0@cluster0.w75zz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  keySession: ["Twój_klucz"],
+  maxAge: 10 * 60 * 1000, // 10 minutes
+};
