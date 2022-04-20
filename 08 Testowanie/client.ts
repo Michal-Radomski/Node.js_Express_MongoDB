df1d5eafa2121e85989ae59e38edf3a631296785
Michal Radomski <michalra@op.pl>
2022-04-20 17:43:48 +0200

    MongoDB testing - commit1

--- 08 Testowanie/client.ts
+++ 08 Testowanie/client.ts
@@ -12,13 +12,16 @@ const dbName = process.env.DBNAME || "test";
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
-  console.log("Connected successfully to server");
+  console.log("Connected successfully to Mongo DB server");
   const db = client.db(dbName);
-  const collection = db.collection("documents");
+  const collectionCars = await db.collection("cars").find().toArray();
+  const collectionTodos = await db.collection("todos").find().toArray();
+  console.log("collectionCars:", collectionCars);
+  console.log("collectionTodos:", collectionTodos);
 
   // the following code examples can be pasted here...
 
-  return "done.";
+  return "Done";
 }
 
 main()
